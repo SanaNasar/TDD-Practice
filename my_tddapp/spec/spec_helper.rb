4999e317c1d726bee9e715b90bbac05bc5749d77
SanaNasar <sanasehereen.cse@gmail.com>
2015-07-25 12:50:21 -0700

    adding specs for my book class

---
+++ my_tddapp/spec/spec_helper.rb
@@ -0,0 +1,6 @@
+# require_relative is similar to require except instead of searching for a ruby path it 
+# searches for inside the directory
+# require_relative '../library'
+require_relative '../book'
+ 
+require 'yaml' #require the database file
